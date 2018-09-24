0b9486069b28b833b9064d26e2e74de8d6f86fa6
4423 <hanamiya4423@gmail.com>
2018-09-24 21:56:03 +0900

    Bootstrap を適用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 import firebase from 'firebase'
 
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false
 
 // Initialize Firebase
